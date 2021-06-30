cdf0dc7845f37207bd5a927580140efa53b842dc
sasilva1998 <sasilva1998@gmail.com>
2021-06-30 03:10:04 -0500

    people gazebo

--- pedsim_gazebo_plugin/src/actor_poses_plugin.cpp
+++ pedsim_gazebo_plugin/src/actor_poses_plugin.cpp
@@ -106,7 +106,7 @@ namespace gazebo
         std::thread rosQueueThread;
         physics::WorldPtr world_;
         event::ConnectionPtr updateConnection_;
-        const float MODEL_OFFSET = 2;
+        const float MODEL_OFFSET = 1.05;
     };
     GZ_REGISTER_WORLD_PLUGIN(ActorPosesPlugin)
 }
