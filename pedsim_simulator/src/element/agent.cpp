3250f93c3fe6217282c5c2dc843ed17c7d3495f2
sasilva1998 <sasilva1998@gmail.com>
2022-02-01 14:11:43 +0000

    feat: frozen agent except

--- pedsim_simulator/src/element/agent.cpp
+++ pedsim_simulator/src/element/agent.cpp
@@ -1,33 +1,33 @@
 /**
-* Copyright 2014 Social Robotics Lab, University of Freiburg
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-*    # Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*    # Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*    # Neither the name of the University of Freiburg nor the names of its
-*       contributors may be used to endorse or promote products derived from
-*       this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*
-* \author Billy Okal <okal@cs.uni-freiburg.de>
-* \author Sven Wehner <mail@svenwehner.de>
-*/
+ * Copyright 2014 Social Robotics Lab, University of Freiburg
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    # Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *    # Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *    # Neither the name of the University of Freiburg nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \author Billy Okal <okal@cs.uni-freiburg.de>
+ * \author Sven Wehner <mail@svenwehner.de>
+ */
 
 #include <pedsim_simulator/agentstatemachine.h>
 #include <pedsim_simulator/config.h>
@@ -158,29 +158,37 @@ Ped::Twaypoint *Agent::updateDestination()
   // ROS_INFO_STREAM("Destinations for this agent [" << destinations.size() << "] ");
 
   // assign new destination
-  if (!destinations.isEmpty())
-  {
-    if (currentDestination != nullptr)
-    {
-      // cycle through destinations
-      Waypoint *previousDestination = destinations.takeFirst();
-      destinations.append(previousDestination);
-    }
 
-    if (isRandom == 0 and frozenStatus != "frozen")
+  try
+  {
+    if (!destinations.isEmpty())
     {
-      currentDestination = destinations.first();
-      // ROS_INFO_STREAM("Destination [" << currentDestination->getName().toStdString() << "] for agent [" << id << "]");
+      if (currentDestination != nullptr)
+      {
+        // cycle through destinations
+        Waypoint *previousDestination = destinations.takeFirst();
+        destinations.append(previousDestination);
+      }
+
+      if (isRandom == 0 and frozenStatus != "frozen")
+      {
+        currentDestination = destinations.first();
+        // ROS_INFO_STREAM("Destination [" << currentDestination->getName().toStdString() << "] for agent [" << id << "]");
+      }
+      else
+      {
+        int iRandomWaypoint = (rand() % static_cast<int>(destinations.size()));
+        // ROS_INFO_STREAM("Random destination agent [" << id << "] reached destination.");
+        // ROS_INFO_STREAM("Random destination number selected [" << iRandomWaypoint << "] for agent.");
+        currentDestination = destinations.at(iRandomWaypoint);
+      }
+      // currentDestination = destinations.first();
     }
-    else
-    {
-      int iRandomWaypoint = (rand() % static_cast<int>(destinations.size()));
-      // ROS_INFO_STREAM("Random destination agent [" << id << "] reached destination.");
-      // ROS_INFO_STREAM("Random destination number selected [" << iRandomWaypoint << "] for agent.");
-      currentDestination = destinations.at(iRandomWaypoint);
-    }
-    //currentDestination = destinations.first();
   }
+  catch (const std::exception &e)
+  {
+  }
+
   return currentDestination;
 }
 
@@ -375,7 +383,7 @@ void Agent::setType(Ped::Tagent::AgentType typeIn)
   emit typeChanged(typeIn);
 }
 
-//random method for random waypoints
+// random method for random waypoints
 void Agent::setRandom(bool randomIn)
 {
   isRandom = randomIn;
